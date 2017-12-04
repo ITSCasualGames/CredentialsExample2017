beaf9bb39224a5e3073c57e20bbcf3c9148a1906
ITSPaul <powell.paul@itsligo.ie>
2017-12-04 12:59:04 +0000

    initial commit

---
+++ CredentialSignalRGameServer/CredentialHub.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using GameData;
+
+namespace CredentialSignalRGameServer
+{
+    public class CredentialHub : Hub
+    {
+        
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+        public PlayerData checkCredentials(string name, string password)
+        {
+            return GameDataObjects.RegisteredPlayers
+                                  .FirstOrDefault(p => p.PlayerName == name && p.Password == password);
+            
+            
+        }
+    }
+}
---
+++ GameData/playerData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameData
+{
+
+    public class PlayerData
+    {
+        public string playerID;
+        public string PlayerName;
+        public string Password;
+        public string GamerTag;
+        public string CharacterImage;
+        public int XP;
+        public PlayerData()
+        {
+
+        }
+
+        
+
+    }
+}
